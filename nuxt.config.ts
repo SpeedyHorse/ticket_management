a575f2ec05c643919952f14b76c31c151152110c
liverHawk <hawkyofkn5365@gmail.com>
2025-09-17 17:25:33 +0900

    tailwind done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ import { baseURL } from "process";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: [
+    '~/assets/css/main.css'
+  ],
   modules: [
     '@nuxt/image',
     '@nuxt/ui',
